b5d0d3cea32a4da35c673950bee55930e9da4b57
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-07-07 14:03:05 +0200

    feat: use podcast artwork to control playback

--- src/ui/PodcastView/EpisodePlayer.svelte
+++ src/ui/PodcastView/EpisodePlayer.svelte
@@ -1,39 +1,43 @@
 <script lang="ts">
-    import { ButtonComponent, Setting, SliderComponent } from "obsidian";
-	import { duration, currentTime, currentEpisode, isPaused, plugin } from "src/store";
+	import { setIcon, SliderComponent } from "obsidian";
+	import {
+		duration,
+		currentTime,
+		currentEpisode,
+		isPaused,
+		plugin,
+	} from "src/store";
 
-    import { formatSeconds } from "src/utility/formatSeconds";
-    import { onDestroy, onMount } from "svelte";
+	import { formatSeconds } from "src/utility/formatSeconds";
+	import { onDestroy, onMount } from "svelte";
 	import { get, Unsubscriber } from "svelte/store";
 
-    let buttonRef: HTMLSpanElement;
-    let playbackRateRef: HTMLSpanElement;
+	let playbackRateRef: HTMLSpanElement;
+	let iconRef: HTMLSpanElement;
 	let unsubscriber: Unsubscriber;
 	let playbackRate: number = get(plugin).settings.defaultPlaybackRate || 1;
 
-    onMount(() => {
+	let isHoveringArtwork: boolean = true;
+
+	function togglePlayback() {
+		isPaused.update((value) => !value);
+	}
+
+	onMount(() => {
 		duration.set(0);
 		currentTime.set(0);
 
-        const buttonComponent = new ButtonComponent(buttonRef);
-        buttonComponent.setIcon("play")
-        buttonComponent.setClass("play-button");
-        buttonComponent.onClick(() => {
-			isPaused.update((value) => {
-				return !value;
-			});
-        });
-
-		unsubscriber = isPaused.subscribe(value => {
-			const btnIcon = value ? "play" : "pause";
-			buttonComponent.setIcon(btnIcon);
+		unsubscriber = isPaused.subscribe((value) => {
+			const btnIcon = value ? "pause" : "play";
+			setIcon(iconRef, btnIcon);
 		});
 
-        const playbackRateComponent = new SliderComponent(playbackRateRef);
-        playbackRateComponent
-            .setLimits(0.5, 4, 0.1)
-            .setValue(playbackRate)
-            .onChange(value => playbackRate = value)
+		const playbackRateComponent = new SliderComponent(playbackRateRef);
+		playbackRateComponent
+			.setLimits(0.5, 3.5, 0.1)
+			.setValue(playbackRate)
+			.onChange((value) => (playbackRate = value));
+	});
 
 	onDestroy(() => unsubscriber());
 
@@ -42,45 +46,125 @@
 		const percent = e.offsetX / progressbar.offsetWidth;
 		currentTime.set(percent * $duration);
 	}
-
 </script>
 
 <div class="episode-player">
-    <div class="image-container">
-        <img 
-            class="podcast-artwork"
-            src={$currentEpisode.artworkUrl}
-            alt={$currentEpisode.title}
-        />
-    </div>
+	<div class="episode-image-container">
+		<div
+			class="hover-container"
+			on:click={togglePlayback}
+			on:mouseenter={() => (isHoveringArtwork = true)}
+			on:mouseleave={() => (isHoveringArtwork = false)}
+		>
+			<img
+				class={"podcast-artwork" +
+					(isHoveringArtwork || $isPaused ? " opacity-50" : "")}
+				src={$currentEpisode.artworkUrl}
+				alt={$currentEpisode.title}
+			/>
+			<div
+				class="podcast-artwork-overlay"
+				style={`display: ${
+					isHoveringArtwork || $isPaused ? "block" : "none"
+				}`}
+			>
+				<span bind:this={iconRef} />
+			</div>
+		</div>
+	</div>
 
-    <h2 class="podcast-title">{$currentEpisode.title}</h2>
+	<h2 class="podcast-title">{$currentEpisode.title}</h2>
 
-    <audio 
-        src={$currentEpisode.streamUrl} 
-        bind:duration={$duration}
-        bind:currentTime={$currentTime}
+	<audio
+		src={$currentEpisode.streamUrl}
+		bind:duration={$duration}
+		bind:currentTime={$currentTime}
 		bind:paused={$isPaused}
-		bind:playbackRate={playbackRate}
-    />
+		bind:playbackRate
+	/>
 
-    <div class="status-container">
-        <span>{formatSeconds($currentTime, "HH:mm:ss")}</span>
-        <progress
-			style="height: 2rem;" 
-			max={$duration} 
-			value={$currentTime} 
+	<div class="status-container">
+		<span>{formatSeconds($currentTime, "HH:mm:ss")}</span>
+		<progress
+			style="height: 2rem;"
+			max={$duration}
+			value={$currentTime}
 			on:click={onClickProgressbar}
 		/>
-        <span>{formatSeconds($duration, "HH:mm:ss")}</span>
-    </div>
+		<span>{formatSeconds($duration, "HH:mm:ss")}</span>
+	</div>
 
-    <div class="play-button-container">
-        <span bind:this={buttonRef} />
-    </div>
-    
-    <div class="controls-container">
+	<div class="controls-container">
 		<span>{playbackRate}x</span>
-   	    <span bind:this={playbackRateRef} />
-    </div>
+		<span bind:this={playbackRateRef} />
+	</div>
 </div>
+
+<style>
+	.episode-player {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+	}
+
+	.episode-image-container {
+		width: 100%;
+		padding: 5% 20%;
+	}
+
+	.hover-container {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.podcast-artwork {
+		width: 100%;
+		height: 100%;
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+	}
+
+	/* Some themes override this, so opting to force like so. */
+	.podcast-artwork:hover {
+		cursor: pointer !important;
+	}
+
+	.podcast-artwork-overlay {
+		position: absolute;
+	}
+
+	.podcast-artwork-overlay:hover {
+		cursor: pointer !important;
+	}
+
+	.opacity-50 {
+		opacity: 0.5;
+	}
+
+	.podcast-title {
+		font-size: 1.5rem;
+		font-weight: bold;
+		margin: 0%;
+		margin-bottom: 0.5rem;
+		text-align: center;
+	}
+
+	.status-container {
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+	}
+
+	.controls-container {
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+		margin-bottom: 2.5rem;
+		flex-direction: column;
+		margin-top: auto;
+	}
+</style>
