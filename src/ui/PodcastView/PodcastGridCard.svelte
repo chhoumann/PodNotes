bb1d9e54faf13912e80d82061a9055303822e33f
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-07-08 13:24:25 +0200

    refactor: separate podcast grid and cards

---
+++ src/ui/PodcastView/PodcastGridCard.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { PodcastFeed } from "src/types/PodcastFeed";
+	import { createEventDispatcher } from "svelte";
+
+	export let feed: PodcastFeed;
+
+	const dispatch = createEventDispatcher();
+
+	function onclickPodcast(feed: PodcastFeed) {
+		dispatch("clickPodcast", { feed });
+	}
+</script>
+
+<img
+    id={feed.title}
+    src={feed.artworkUrl}
+    alt={feed.title}
+    on:click={onclickPodcast.bind(null, feed)}
+    class={`podcast-image`}
+/>
+
+<style>
+	.hover-container {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.podcast-image {
+		width: 100%;
+		cursor: pointer !important;
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+	}
+</style>
