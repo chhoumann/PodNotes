3e5585d8308c3198b5968c0a75a721994d4c9c60
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-07-12 07:56:02 +0200

    fix: add missing styles property to slider component

--- src/ui/obsidian/Slider.svelte
+++ src/ui/obsidian/Slider.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
     import { SliderComponent } from "obsidian";
+    import { CSSObject } from "src/types/CSSObject";
+    import extractStylesFromObj from "src/utility/extractStylesFromObj";
     import { afterUpdate, createEventDispatcher, onMount } from "svelte";
 
     export let value: number;
     export let limits: [min: number, max: number] | [min: number, max: number, step: number];
+    export { styles as style };
 
     let sliderRef: HTMLSpanElement;
 
     const dispatch = createEventDispatcher();
 
     let slider: SliderComponent;
+    let styles: CSSObject;
 
     // This is not a complete implementation. I implemented what I needed.
 
@@ -26,7 +30,10 @@
     function updateSliderAttributes(sldr: SliderComponent) {
         if (value) sldr.setValue(value);
         if (limits) sldr.setLimits.apply(sldr, limits);
-        
+        if (styles) {
+            sldr.sliderEl.setAttr("style", extractStylesFromObj(styles));
+        }
+
         sldr.onChange((value: number) => {
             dispatch("change", { value });
         });
