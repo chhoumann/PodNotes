6af28505e3ac3f65a9b0e092bed9711e188c8937
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-07-19 13:06:54 +0200

    refactor: rename iTunes API consumer

--- src/ui/settings/PodcastQueryGrid.svelte
+++ src/ui/settings/PodcastQueryGrid.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-    import { debounce } from "obsidian";
-    import { consume } from "src/iTunesAPIConsumer";
-    import { savedFeeds } from "src/store";
-    import { PodcastFeed } from "src/types/PodcastFeed";
-    import Text from "../obsidian/Text.svelte";
-    import PodcastResultCard from "./PodcastResultCard.svelte";
+	import { debounce } from "obsidian";
+	import { queryiTunesPodcasts} from "src/iTunesAPIConsumer";
+	import { savedFeeds } from "src/store";
+	import { PodcastFeed } from "src/types/PodcastFeed";
+	import Text from "../obsidian/Text.svelte";
+	import PodcastResultCard from "./PodcastResultCard.svelte";
 
-    let searchResults: PodcastFeed[] = [];
-    let gridSizeClass: string = "grid-3";
+	let searchResults: PodcastFeed[] = [];
+	let gridSizeClass: string = "grid-3";
 
 	if (searchResults.length % 3 === 0 || searchResults.length > 3) {
 		gridSizeClass = "grid-3";
@@ -17,80 +17,80 @@
 		gridSizeClass = "grid-1";
 	}
 
-    const debouncedUpdate = debounce(async (event: CustomEvent<{value: string}>) => {
-        searchResults = await consume(event.detail.value);
-    }, 300, true);
+	const debouncedUpdate = debounce(
+		async (event: CustomEvent<{ value: string }>) => {
+			searchResults = await queryiTunesPodcasts(event.detail.value);
+		},
+		300,
+		true
+	);
 
-    function addPodcast(event: CustomEvent<{ podcast: PodcastFeed }>) {
-        const { podcast } = event.detail;
+	function addPodcast(event: CustomEvent<{ podcast: PodcastFeed }>) {
+		const { podcast } = event.detail;
 
-        savedFeeds.update(feeds => ({ ...feeds, [podcast.title]: podcast }));
-    }
+		savedFeeds.update((feeds) => ({ ...feeds, [podcast.title]: podcast }));
+	}
 
-    function removePodcast(event: CustomEvent<{ podcast: PodcastFeed }>) {
-        const { podcast } = event.detail;
+	function removePodcast(event: CustomEvent<{ podcast: PodcastFeed }>) {
+		const { podcast } = event.detail;
 
-        savedFeeds.update(feeds => {
-            const newFeeds = { ...feeds };
-            delete newFeeds[podcast.title];
-            return newFeeds;
-        });
-    }
+		savedFeeds.update((feeds) => {
+			const newFeeds = { ...feeds };
+			delete newFeeds[podcast.title];
+			return newFeeds;
+		});
+	}
 </script>
 
 <div class="podcast-query-container">
     <h3 class="podcast-query-heading">Search for a podcast</h3>
-    <Text 
-        placeholder="Search..."
-        on:change={debouncedUpdate}
-        style={{
-            width: "100%",
-            "margin-bottom": "1rem",
-        }}
-    />
+	<Text
+		placeholder="Search..."
+		on:change={debouncedUpdate}
+		style={{
+			width: "100%",
+			"margin-bottom": "1rem",
+		}}
+	/>
 
-    <div 
-        class={`
+	<div
+		class={`
             podcast-query-results
             ${gridSizeClass}
         `}
-    >
-        {#each searchResults as podcast}
-            <PodcastResultCard 
-                podcast={podcast} 
-                isSaved={$savedFeeds[podcast.title] !== undefined}
-                on:addPodcast={addPodcast}
-                on:removePodcast={removePodcast}
-            />
-        {/each}
-    </div>
+	>
+		{#each searchResults as podcast}
+			<PodcastResultCard
+				{podcast}
+				isSaved={$savedFeeds[podcast.title] !== undefined}
+				on:addPodcast={addPodcast}
+				on:removePodcast={removePodcast}
+			/>
+		{/each}
+	</div>
 </div>
 
 <style>
-    .podcast-query-container {
-        margin-bottom: 2rem;
-    }
+	.podcast-query-container {
+		margin-bottom: 2rem;
+	}
 
-    .podcast-query-heading {
-        margin-bottom: 0.5rem;
-    }
+	.podcast-query-results {
+		width: 100%;
+		height: 100%;
+		display: grid;
+		grid-gap: 1rem;
+	}
 
-    .podcast-query-results {
-        width: 100%;
-        height: 100%;
-        display: grid;
-        grid-gap: 1rem;
-    }
+	.grid-3 {
+		grid-template-columns: repeat(3, 1fr);
+	}
 
-    .grid-3 {
-        grid-template-columns: repeat(3, 1fr);
-    }
+	.grid-2 {
+		grid-template-columns: repeat(2, 1fr);
+	}
 
-    .grid-2 {
-        grid-template-columns: repeat(2, 1fr);
-    }
-
-    .grid-1 {
-        grid-template-columns: repeat(1, 1fr);
-    }
+	.grid-1 {
+		grid-template-columns: repeat(1, 1fr);
+	}
 </style>
